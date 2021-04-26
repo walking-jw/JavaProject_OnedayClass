f4b9808e5d82665307842a62dcaabba9a2ff2d59
Hyoeun-Kwon <81559760+Hyoeun-Kwon@users.noreply.github.com>
2021-04-26 17:12:36 +0900

    4.26 Test 1
    Class make

---
+++ OnedayClass/src/com/javaproject/classlist/Category.java
@@ -0,0 +1,43 @@
+package com.javaproject.classlist;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Category {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Category window = new Category();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Category() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 560, 625);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
