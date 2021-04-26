ba93ff90fbd0dd27090cd0b7b79029eee7ac9e30
norsk-Gh <81655952+norsk-Gh@users.noreply.github.com>
2021-04-26 15:49:31 +0900

    Project Outline
    project outline
    
    Team 2 :
    Hyeji Jo
    Doyoung Lee
    Hyoeun Kwon
    Jaewon Park
    
    2021.04.26, 15:48
    Park Jaewon

---
+++ OnedayClass/src/com/javaproject/base/Main.java
@@ -0,0 +1,148 @@
+package com.javaproject.base;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Main {
+
+	private JFrame frame;
+	private JButton btnStart;
+	private JLabel lblNewLabel;
+	private JButton btnDoyoung;
+	private JButton btnHyeji;
+	private JButton btnHyoeun;
+	private JButton btnJaewon;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 560, 625);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		frame.getContentPane().add(getBtnStart());
+		frame.getContentPane().add(getLblNewLabel());
+		frame.getContentPane().add(getBtnDoyoung());
+		frame.getContentPane().add(getBtnHyeji());
+		frame.getContentPane().add(getBtnHyoeun());
+		frame.getContentPane().add(getBtnJaewon());
+	}
+	private JButton getBtnStart() {
+		if (btnStart == null) {
+			btnStart = new JButton("START");
+			btnStart.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					
+				}
+			});
+			btnStart.setBounds(216, 355, 117, 29);
+		}
+		return btnStart;
+	}
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("금손양성소");
+			lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 38));
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel.setBounds(133, 215, 277, 67);
+		}
+		return lblNewLabel;
+	}
+	private JButton getBtnDoyoung() {
+		if (btnDoyoung == null) {
+			btnDoyoung = new JButton("Doyoung Start");
+			btnDoyoung.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					Signin category = new Signin();
+					category.setVisible_Signin();
+					frame.dispose();
+					
+				}
+			});
+			btnDoyoung.setBounds(51, 407, 117, 29);
+		}
+		return btnDoyoung;
+	}
+	private JButton getBtnHyeji() {
+		if (btnHyeji == null) {
+			btnHyeji = new JButton("Hyeji Start");
+			btnHyeji.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					StudentMylecture category = new StudentMylecture();
+					category.setVisible_StudentMylecture();
+					frame.dispose();
+		
+				}
+			});
+			btnHyeji.setBounds(182, 407, 117, 29);
+		}
+		return btnHyeji;
+	}
+	private JButton getBtnHyoeun() {
+		if (btnHyoeun == null) {
+			btnHyoeun = new JButton("Hyoeun Start");
+			btnHyoeun.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					Category category = new Category();
+					category.setVisible_Category();
+					frame.dispose();
+					
+					
+				}
+			});
+			btnHyoeun.setBounds(311, 407, 117, 29);
+		}
+		return btnHyoeun;
+	}
+	private JButton getBtnJaewon() {
+		if (btnJaewon == null) {
+			btnJaewon = new JButton("Jaewon Start");
+			btnJaewon.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+					TeacherMypage category = new TeacherMypage();
+					category.setVisible_TeacherMypage();
+					frame.dispose();
+					
+				}
+			});
+			btnJaewon.setBounds(437, 407, 117, 29);
+		}
+		return btnJaewon;
+	}
+}
---
+++ OnedayClass/src/com/javaproject/base/ShareVar.java
@@ -0,0 +1,70 @@
+package com.javaproject.base;
+
+public class ShareVar {
+	
+		
+							/* * * * * * * * * * * * *
+							 *  DATABASE SETTING 	 *
+							 *  - 2021.04.26, 15:15  *
+							 *  - Park Jaewon  	     *
+							 * * * * * * * * * * * * *  
+							 */
+	
+		/* >>>>>>>> 작업시 착안사항 [ 1 ] <<<<<<<<
+		 * Main(금손양성소) 페이지에서 각자 자기 버튼 눌러서 시작하고 작업해주시면 됩니다.
+		 * 하단에 지정해주신 이름으로 클라스를 만들고 + 착안사항 [ 3 ]의 Method를 추가해주시고 시작해주세요 ~!^^
+		 * 
+		 *  도영님 : Doyoung start 버튼 ---> Signin.java
+		 *  혜지님 : Hyeji start 버튼 ---> StudentMylecture.java
+		 *  효은님 : Hyoeun start 버튼 ---> Category.java
+		 *  재원님 : Jaewon start 버튼 ---> TeacherMypage.java
+		 *  
+		 */
+		
+		/*
+		 *  >>>>>>>> 작업시 착안사항 [ 2 ] <<<<<<<<	
+		 *  작업하실때 자신의 IP 입력후 작업해주시고 Merge할 때 꼭!!! IP 로 다시 돌려주시고 주석처리 하셔야합니다.
+		 *  
+		 */ 
+			
+		/*  >>>>>>>> 작업시 착안사항 [ 3 ] <<<<<<<<	
+		 *  // 도영님 복사
+		 *  public void setVisible_Signin(boolean i){
+		 *  	frame.setVisible(i);
+		 *  }
+		 *  
+		 *  // 혜지님 복사
+		 *  public void setVisible_StudenMylecture(boolean i){
+		 *  	frame.setVisible(i);
+		 *  }
+		 *  
+		 *  // 효은님 복사
+		 *  public void Category(boolean i){
+		 *  	frame.setVisible(i);
+		 *  }
+		 *  
+		 */
+	
+	// 효은
+//	public static final String url_mysql = "jdbc:mysql://IP/OnedayClass?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+
+	// 혜지
+//	public static final String url_mysql = "jdbc:mysql://IP/OnedayClass?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	
+	// 도영
+//	public static final String url_mysql = "jdbc:mysql://IP/OnedayClass?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	
+	// 재원
+//	public static final String url_mysql = "jdbc:mysql://IP/OnedayClass?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	
+	// 공용
+	public static final String id_mysql = "root";
+	public static final String pw_mysql = "qwer1234";
+	
+	public static String currentuser = "";
+	
+	// ** "WHERE studentid = " + ShareVar.currentuser
+	
+	
+	
+}
