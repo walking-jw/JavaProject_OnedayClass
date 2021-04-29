d3ea7c0194c0e262a9f54cbed7e95c9d692d89d8
Hyoeun-Kwon <81559760+Hyoeun-Kwon@users.noreply.github.com>
2021-04-29 12:02:23 +0900

    2021-04-29 (12:01)
    Update( class info -attend Ok)

--- OnedayClass/src/com/javaproject/classinfo/Classinfo.java
+++ OnedayClass/src/com/javaproject/classinfo/Classinfo.java
@@ -1,34 +1,42 @@
 package com.javaproject.classinfo;
 
+import java.awt.Color;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+
+import com.javaproject.classlist.DbAction;
 
 public class Classinfo {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JLabel lblcName;
-	private JLabel lbltName;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
 	private JTextField tfcName;
-	private JTextField tftName;
 	private JTextField tfcLocation;
-	private JTextField textField_1;
-	private JLabel lblcTime2;
-	private JLabel lblcLocation;
-	private JLabel lblcTime;
-	private JLabel lblcDate;
-	private JTextField textField_2;
-	private JLabel lblcContents;
-	private JTextField textField_3;
-	private JButton btnNewButton;
-	private JTextField tfcPrice;
+	private JTextField tfcDate;
+	private JTextField tfcTime;
+	private JButton btntName;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_3_1;
+	private JTextField tfcContents;
+	private JButton btnReview;
 	private JButton btnAttend;
+	private JLabel lblNewLabel_6;
+	private JTextField tfcPrice;
 
 	/**
 	 * Launch the application.
@@ -58,161 +66,236 @@ public class Classinfo {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.addWindowListener(new WindowAdapter() {
+		
+			@Override
+			public void windowOpened(WindowEvent e) {
+				//window 시작시 정보를 불러와라!
+				
+				ClassInfo();
+
+			}
+		});
 		frame.setTitle("금손양성소");
 		frame.setBounds(100, 100, 560, 625);
+		frame.setLocationRelativeTo(frame);  //  --------------- 가운데로 보내기
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(getTextField());
-		frame.getContentPane().add(getLblcName());
-		frame.getContentPane().add(getLbltName());
+		frame.getContentPane().add(getLblNewLabel());
+		frame.getContentPane().add(getLblNewLabel_1());
+		frame.getContentPane().add(getLblNewLabel_2());
+		frame.getContentPane().add(getLblNewLabel_3());
+		frame.getContentPane().add(getLblNewLabel_4());
 		frame.getContentPane().add(getTfcName());
-		frame.getContentPane().add(getTftName());
 		frame.getContentPane().add(getTfcLocation());
-		frame.getContentPane().add(getTextField_1());
-		frame.getContentPane().add(getLblcTime2());
-		frame.getContentPane().add(getLblcLocation());
-		frame.getContentPane().add(getLblcTime());
-		frame.getContentPane().add(getLblcDate());
-		frame.getContentPane().add(getTextField_2());
-		frame.getContentPane().add(getLblcContents());
-		frame.getContentPane().add(getTextField_3());
-		frame.getContentPane().add(getBtnNewButton());
-		frame.getContentPane().add(getTfcPrice());
+		frame.getContentPane().add(getTfcDate());
+		frame.getContentPane().add(getTfcTime());
+		frame.getContentPane().add(getBtntName());
+		frame.getContentPane().add(getLblNewLabel_5());
+		frame.getContentPane().add(getLblNewLabel_3_1());
+		frame.getContentPane().add(getTfcContents());
+		frame.getContentPane().add(getBtnReview());
 		frame.getContentPane().add(getBtnAttend());
+		frame.getContentPane().add(getLblNewLabel_6());
+		frame.getContentPane().add(getTfcPrice());
 	}
-	private JTextField getTextField() {
-		if (textField == null) {
-			textField = new JTextField();
-			textField.setBounds(24, 6, 510, 210);
-			textField.setColumns(10);
+	private JLabel getLblNewLabel() {
+		if (lblNewLabel == null) {
+			lblNewLabel = new JLabel("강의명");
+			lblNewLabel.setBounds(30, 206, 61, 16);
 		}
-		return textField;
+		return lblNewLabel;
 	}
-	private JLabel getLblcName() {
-		if (lblcName == null) {
-			lblcName = new JLabel("강의명");
-			lblcName.setBounds(24, 233, 61, 16);
+	private JLabel getLblNewLabel_1() {
+		if (lblNewLabel_1 == null) {
+			lblNewLabel_1 = new JLabel("강사");
+			lblNewLabel_1.setBounds(30, 243, 61, 16);
 		}
-		return lblcName;
+		return lblNewLabel_1;
 	}
-	private JLabel getLbltName() {
-		if (lbltName == null) {
-			lbltName = new JLabel("강사명");
-			lbltName.setBounds(24, 263, 61, 16);
+	private JLabel getLblNewLabel_2() {
+		if (lblNewLabel_2 == null) {
+			lblNewLabel_2 = new JLabel("장소");
+			lblNewLabel_2.setBounds(30, 284, 61, 16);
 		}
-		return lbltName;
+		return lblNewLabel_2;
+	}
+	private JLabel getLblNewLabel_3() {
+		if (lblNewLabel_3 == null) {
+			lblNewLabel_3 = new JLabel("강의시간");
+			lblNewLabel_3.setBounds(30, 324, 61, 16);
+		}
+		return lblNewLabel_3;
+	}
+	private JLabel getLblNewLabel_4() {
+		if (lblNewLabel_4 == null) {
+			lblNewLabel_4 = new JLabel("날짜");
+			lblNewLabel_4.setBounds(30, 358, 61, 16);
+		}
+		return lblNewLabel_4;
 	}
 	private JTextField getTfcName() {
 		if (tfcName == null) {
 			tfcName = new JTextField();
 			tfcName.setEditable(false);
-			tfcName.setBounds(127, 228, 394, 26);
+			tfcName.setBounds(103, 201, 394, 26);
 			tfcName.setColumns(10);
 		}
 		return tfcName;
-	}
-	private JTextField getTftName() {
-		if (tftName == null) {
-			tftName = new JTextField();
-			tftName.setEditable(false);
-			tftName.setColumns(10);
-			tftName.setBounds(127, 258, 198, 26);
-		}
-		return tftName;
 	}
 	private JTextField getTfcLocation() {
 		if (tfcLocation == null) {
 			tfcLocation = new JTextField();
 			tfcLocation.setEditable(false);
 			tfcLocation.setColumns(10);
-			tfcLocation.setBounds(127, 296, 394, 26);
+			tfcLocation.setBounds(103, 274, 394, 26);
 		}
 		return tfcLocation;
 	}
-	private JTextField getTextField_1() {
-		if (textField_1 == null) {
-			textField_1 = new JTextField();
-			textField_1.setEditable(false);
-			textField_1.setColumns(10);
-			textField_1.setBounds(127, 334, 38, 26);
+	private JTextField getTfcDate() {
+		if (tfcDate == null) {
+			tfcDate = new JTextField();
+			tfcDate.setEditable(false);
+			tfcDate.setColumns(10);
+			tfcDate.setBounds(103, 353, 394, 26);
 		}
-		return textField_1;
+		return tfcDate;
 	}
-	private JLabel getLblcTime2() {
-		if (lblcTime2 == null) {
-			lblcTime2 = new JLabel("시간");
-			lblcTime2.setBounds(172, 339, 29, 16);
+	private JTextField getTfcTime() {
+		if (tfcTime == null) {
+			tfcTime = new JTextField();
+			tfcTime.setBounds(103, 319, 39, 26);
+			tfcTime.setColumns(10);
 		}
-		return lblcTime2;
+		return tfcTime;
 	}
-	private JLabel getLblcLocation() {
-		if (lblcLocation == null) {
-			lblcLocation = new JLabel("장소");
-			lblcLocation.setBounds(24, 302, 61, 16);
+	private JButton getBtntName() {
+		if (btntName == null) {
+			btntName = new JButton("");
+			btntName.addActionListener(new ActionListener() {
+				
+			
+				public void actionPerformed(ActionEvent e) {
+					///강사 정보와 QnA창이 뜨게 하기
+					TeacherInfo_QnA tInfo_QnA = new TeacherInfo_QnA();
+					tInfo_QnA.setVisible_tInfo_QnA(true);
+					frame.dispose();
+				}
+			});
+			btntName.setBackground(new Color(240, 230, 140));
+			btntName.setBounds(102, 238, 117, 29);
 		}
-		return lblcLocation;
+		return btntName;
 	}
-	private JLabel getLblcTime() {
-		if (lblcTime == null) {
-			lblcTime = new JLabel("강의시간");
-			lblcTime.setBounds(24, 334, 61, 16);
+	private JLabel getLblNewLabel_5() {
+		if (lblNewLabel_5 == null) {
+			lblNewLabel_5 = new JLabel("시간");
+			lblNewLabel_5.setBounds(145, 324, 30, 16);
 		}
-		return lblcTime;
+		return lblNewLabel_5;
 	}
-	private JLabel getLblcDate() {
-		if (lblcDate == null) {
-			lblcDate = new JLabel("날짜");
-			lblcDate.setBounds(24, 367, 61, 16);
+	private JLabel getLblNewLabel_3_1() {
+		if (lblNewLabel_3_1 == null) {
+			lblNewLabel_3_1 = new JLabel("강의소개");
+			lblNewLabel_3_1.setBounds(30, 395, 61, 16);
 		}
-		return lblcDate;
+		return lblNewLabel_3_1;
 	}
-	private JTextField getTextField_2() {
-		if (textField_2 == null) {
-			textField_2 = new JTextField();
-			textField_2.setEditable(false);
-			textField_2.setColumns(10);
-			textField_2.setBounds(127, 362, 198, 26);
+	private JTextField getTfcContents() {
+		if (tfcContents == null) {
+			tfcContents = new JTextField();
+			tfcContents.setEditable(false);
+			tfcContents.setBounds(30, 423, 505, 82);
+			tfcContents.setColumns(10);
 		}
-		return textField_2;
+		return tfcContents;
 	}
-	private JLabel getLblcContents() {
-		if (lblcContents == null) {
-			lblcContents = new JLabel("강의 소개");
-			lblcContents.setFont(new Font("Lucida Grande", Font.BOLD, 15));
-			lblcContents.setBounds(24, 395, 61, 16);
+	private JButton getBtnReview() {
+		if (btnReview == null) {
+			btnReview = new JButton("후기");
+			btnReview.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					//해당 강의에 대한 후기 나오게 하기
+					ClassReview classreview = new ClassReview();
+					classreview.setVisible_ClassReview(true);
+					frame.dispose();
+				}
+			});
+			btnReview.setBounds(418, 517, 117, 29);
 		}
-		return lblcContents;
-	}
-	private JTextField getTextField_3() {
-		if (textField_3 == null) {
-			textField_3 = new JTextField();
-			textField_3.setBounds(24, 417, 510, 70);
-			textField_3.setColumns(10);
-		}
-		return textField_3;
-	}
-	private JButton getBtnNewButton() {
-		if (btnNewButton == null) {
-			btnNewButton = new JButton("후기");
-			btnNewButton.setBounds(416, 499, 117, 29);
-		}
-		return btnNewButton;
-	}
-	private JTextField getTfcPrice() {
-		if (tfcPrice == null) {
-			tfcPrice = new JTextField();
-			tfcPrice.setEditable(false);
-			tfcPrice.setHorizontalAlignment(SwingConstants.TRAILING);
-			tfcPrice.setBounds(297, 540, 117, 26);
-			tfcPrice.setColumns(10);
-		}
-		return tfcPrice;
+		return btnReview;
 	}
 	private JButton getBtnAttend() {
 		if (btnAttend == null) {
 			btnAttend = new JButton("수강신청");
-			btnAttend.setBounds(417, 540, 117, 29);
+			btnAttend.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					/*/
+					 * 2021-04-27 권효은
+					 * 수강신청 버튼 누르면 '신청하겠습니까?'창 나오기
+					 *  
+					 */
+					
+					AttendCheck attendCheck = new AttendCheck();
+					attendCheck.setVisible_AttendCheck(true);
+					frame.dispose();
+					
+				}
+			});
+			btnAttend.setBackground(Color.BLUE);
+			btnAttend.setBounds(418, 558, 117, 29);
 		}
 		return btnAttend;
 	}
+	
+	
+	/*/
+	 * 
+	 */
+	public void setVisible_Classinfo(boolean h) {
+		frame.setVisible(h);
+		
+	}
+
+
+	// -> 해당 강의에 대한 세부정보 가져오기
+	public void ClassInfo() {
+		
+		int cid = DbAction.classid;
+		
+		DbActionInfo dbActioninfo = new DbActionInfo(cid);
+		Bean2 bean2 = dbActioninfo.ButtonClassInfo();
+		
+		tfcName.setText(bean2.getcName());
+		btntName.setText(bean2.gettName());
+		tfcLocation.setText(bean2.getcLocation());
+		tfcTime.setText(bean2.getcTime());
+		tfcDate.setText(bean2.getcDate());
+		tfcContents.setText(bean2.getcContents());
+		tfcPrice.setText(Integer.toString(bean2.getcPrice()));
+		
+		
+	}//ClassInfo End
+	private JLabel getLblNewLabel_6() {
+		if (lblNewLabel_6 == null) {
+			lblNewLabel_6 = new JLabel("");
+			lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel_6.setBounds(30, 17, 505, 158);
+		}
+		return lblNewLabel_6;
+	}
+	private JTextField getTfcPrice() {
+		if (tfcPrice == null) {
+			tfcPrice = new JTextField();
+			tfcPrice.setText("0");
+			tfcPrice.setHorizontalAlignment(SwingConstants.TRAILING);
+			tfcPrice.setEditable(false);
+			tfcPrice.setBounds(286, 558, 130, 26);
+			tfcPrice.setColumns(10);
+		}
+		return tfcPrice;
+	}
+	
+	// ------- 이미지 test
+
 }
