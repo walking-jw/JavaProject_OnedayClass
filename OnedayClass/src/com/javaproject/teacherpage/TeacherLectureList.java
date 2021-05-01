119febae72ba695266b9866c9f8e78139212bd0b
hyehh <81559324+hyehh@users.noreply.github.com>
2021-05-02 03:55:02 +0900

    2021.05.02 Hyeji Third Commit

--- OnedayClass/src/com/javaproject/teacherpage/TeacherLectureList.java
+++ OnedayClass/src/com/javaproject/teacherpage/TeacherLectureList.java
@@ -70,7 +70,7 @@ import java.awt.event.ActionEvent;
 					SearchActionBefore();
 				}
 			});
-			frame.setTitle("강의관리");
+			frame.setTitle("나의 강의");
 			frame.setBounds(100, 100, 560, 625);
 			frame.setLocationRelativeTo(frame);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
